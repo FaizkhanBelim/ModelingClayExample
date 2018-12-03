b55c15d7b4f8b5f97d671795b52f30c69c7261a2
Faiz <faiz@thinkbiz.co.in>
2018-12-03 15:11:04 +0530

    uplaod files change

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/modelingClayExample'));
+app.use(express.static(__dirname + '/dist/modelingclayexample'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/modelingClayExample/index.html'));
+res.sendFile(path.join(__dirname+'/dist/modelingclayexample/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
