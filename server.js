16cd5fcb00800c08f9d2ad259eee1a0069f98852
Faiz <faiz@thinkbiz.co.in>
2018-12-03 16:08:23 +0530

    new msg

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
-//Install express server
-const express = require('express');
+const compression = require('compression');
 const path = require('path');
-
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 8080;
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/modelingclayexample'));
+// Gzip
+app.use(compression());
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/modelingclayexample/index.html'));
-});
+// Run the app by serving the static files in the dist directory
+app.use(express.static(__dirname + '/dist'));
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port);
+
+// For all GET requests, send back index.html so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
